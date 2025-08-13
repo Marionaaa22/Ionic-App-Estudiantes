daa39ae2bf249878f2fd8990fd890ea05bb50214
Marionaaa22 <mariona.claros@mataro.epiaedu.cat>
2025-08-13 17:53:59 +0200

    creacion pagina inicial

--- AppEstudiantes/src/app/home/home.page.ts
+++ AppEstudiantes/src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon,IonButton, IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,
+    IonCardContent,IonFab, IonFabButton} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
+  standalone: true,
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton,IonCard,IonCardHeader,IonCardTitle,
+    IonCardSubtitle,IonCardContent,IonFab,IonFabButton, ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ add });
+  }
 }
